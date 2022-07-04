740748e1900b1ec400d127e664cc32f028577236
Jung_Jihoon <coutinho10ace@gmail.com>
2022-07-04 12:50:54 +0900

    programmers day3

--- Data/ProgrammersLV1.py
+++ Data/ProgrammersLV1.py
@@ -25,3 +25,17 @@ def solution(arr1, arr2):
 def solution(phone_number):
     phone_number = phone_number.replace(phone_number[:-4],'*'*len(phone_number[:-4]))
     return phone_number
+
+#양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+def solution(x):
+    a=str(x)
+    b=[]
+    for i in range(len(a)):
+        b.append(a[i])
+    c=list(map(int,b))
+    d=sum(c)
+    return int(a)%d==0
+
+#정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+def solution(arr):
+    return sum(arr)/len(arr)
