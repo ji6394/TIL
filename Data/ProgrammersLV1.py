44bd1bee06dd1fb7f26f103d5205c14ddf73b541
Jung_Jihoon <coutinho10ace@gmail.com>
2022-07-08 21:13:35 +0900

    0708

--- Data/ProgrammersLV1.py
+++ Data/ProgrammersLV1.py
@@ -144,3 +144,19 @@ def solution(s):
                 answer += i[j].lower()
         answer+= ' '
     return answer[0:-1]
+#정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+def solution(n):
+    a=[]
+    for i in range(1,n+1):
+        if n%i == 0:
+            a.append(i)
+    return sum(a)
+#어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+def solution(s, n):
+    s=list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+n)%26+ord('a'))
+    return ''.join(s)
