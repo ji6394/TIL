77c14a065a52c3a4d371cac4594b15e61dea23f1
Jung_Jihoon <coutinho10ace@gmail.com>
2022-07-11 18:39:15 +0900

    0711

--- Data/ProgrammersLV1.py
+++ Data/ProgrammersLV1.py
@@ -201,3 +201,27 @@ def solution(n):
                 break
             a[idx]=1
     return a[2:].count(0)
+#String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+def solution(seoul):
+    position = seoul.index('Kim')
+    answer = '김서방은 %d에 있다' % position
+    return answer
+#문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+def solution(s):
+    a = (len(s)==4) or (len(s))==6
+    b=True
+    try:
+        int(s)
+    except:
+        b=False
+    return a and b
+#문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+#s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+def solution(s):
+    s=list(s)
+    for i in range(len(s)):
+        s[i]=ord(s[i])
+    s.sort(reverse=True)
+    for i in range(len(s)):
+        s[i]=chr(s[i])
+    return ''.join(s)
