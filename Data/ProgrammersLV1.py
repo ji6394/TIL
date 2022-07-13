b9e1001d7171ecd15c4ab364c35a7a5d3350d611
Jung_Jihoon <coutinho10ace@gmail.com>
2022-07-13 16:07:21 +0900

    0713

--- Data/ProgrammersLV1.py
+++ Data/ProgrammersLV1.py
@@ -225,3 +225,19 @@ def solution(s):
     for i in range(len(s)):
         s[i]=chr(s[i])
     return ''.join(s)
+#대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+#예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+def solution(s):
+    pn = s.count('p')+s.count('P')
+    yn = s.count('y')+s.count('Y')
+    answer=True
+    if pn != yn:
+        answer = False
+    return answer
+#문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+import numpy as np
+def solution(strings, n):
+    strings.sort()
+    return sorted(strings, key=lambda x:x[n])
+    
