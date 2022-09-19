4a3bd58df88430b1765d021585a23c169b86e06d
Jung_Jihoon <coutinho10ace@gmail.com>
2022-09-19 15:06:00 +0900

    rw

--- Lecture/FirstAI/lec6.py
+++ Lecture/FirstAI/lec6.py
@@ -1,5 +1,6 @@
 #Pandas
 import pandas as pd
+df_data=[1,2,3]
 df_data.values
 from pandas import Series, DataFrame
 import pandas as pd
@@ -7,16 +8,18 @@ list_data = [1,2,3,4,5]
 example_obj = Series(data = list_data)
 example_obj
 
+raw_data = [11,2,3,4,5,6]
 DataFrame(raw_data, columns=['age','city']) #특정 칼럼만 선택하여 로드하기
-DataFrame(raw_data, columns=['first_name','last_name','age','city','debt'] #새로운 칼럼 추가하기
+DataFrame(raw_data, columns=['first_name','last_name','age','city','debt']) #새로운 칼럼 추가하기
 
 #indexing
+df=[1,2,3]
 df.loc[1]
 df['age'].iloc[1:]
 
 df.T #전치행렬
 df.values #값 출력
-df.to)csv() #csv로 변환
+df.to_csv() #csv로 변환
 
 del df['debt'] #칼럼 삭제
 df.drop(1) #행 삭제
@@ -29,7 +32,7 @@ df[:3] #정수형으로 인덱싱하면 Row에 적용. 문자형으로 인덱싱
 
 #Series Operation
 s1 = Series(range(1,6), index=list('abcde'))
-s2 = Series(range(5,11), index=list['bcedef'])
+s2 = Series(range(5,11), index=list('cdefg'))
 s1.add(s2) #인덱스가 같은 것끼리 연산 수행
 s1+s2 #위와 동일
 
