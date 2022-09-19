d1123fb0f44e144a7d7aeaea9d7a7906cda91559
Jung_Jihoon <coutinho10ace@gmail.com>
2022-09-20 06:53:08 +0900

    rw

--- Data/Kaggle/porto_seguro.py
+++ Data/Kaggle/porto_seguro.py
@@ -4,9 +4,11 @@
 #0과 1 중 1이 적기 때문에 1의 개수를 늘려주는 것을 오버샘플링이라 한다.
 #선택은 자유이지만 오버샘플링의 경우 데이터 셋의 크기가 늘어난다는 점이 있음을 인지하자
 
+import random
 #언더샘플링 비율을 지정해주기 위하여
 desired_apriori = 0.1
 #target 변수의 클래스에 따른 인덱스 지정
+df_train=[1,2,3,4,5]
 idx_0 = df_train[df_train['target']==0].index
 idx_1 = df_train[df_train['target']==1].index
 
@@ -25,7 +27,7 @@ print(nb_0)
 print(undersampled_nb_0)
 
 #언더샘플링 비율이 적용된 개수만큼 랜덤하게 샘플을 뽑아서 그 인덱스 저장
-undersampled_idx = shuffle(idx_0, random_state = 37, n_samples = undersampled_nb_0)
+undersampled_idx = random.shuffle(idx_0, random_state = 37, n_samples = undersampled_nb_0)
 
 #언더샘플링 인덱스와 클래스 1의 인덱스를 리스트로 저장
 idx_list = list(undersampled_idx)+list(idx_1)
