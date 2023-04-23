3d59e3c58a70497b1442c33c1ba2435df40e8080
Jung_Jihoon <coutinho10ace@gmail.com>
2023-04-23 20:51:12 +0900

    코딩테스트 공부

--- Programmers/etc.py
+++ Programmers/etc.py
@@ -18,3 +18,24 @@ def solution(absolutes, signs):
         else:
             answer -= absolutes[i]
     return answer
+
+#로또의 최고최소순위
+def solution(lottos, win_nums):
+    lottos = list(lottos)
+    win_nums = list(win_nums)
+    zero_num = lottos.count(0)
+    while 0 in lottos:
+        lottos.remove(0)
+    counting=0
+    for i in lottos:
+        if i in win_nums:
+            counting+=1
+    max_count = counting+zero_num
+    min_count = counting
+    answer=[]
+    for i in [max_count, min_count] :
+        if i>=2:
+            answer.append(7-i)
+        else:
+            answer.append(6)
+    return answer
