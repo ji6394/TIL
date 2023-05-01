97bbbc658ec4651e0242c9ea6bf04bc0d78bf4a6
Jung_Jihoon <coutinho10ace@gmail.com>
2023-05-01 10:54:45 +0900

    신고 결과 받기

--- Programmers/kakao.py
+++ Programmers/kakao.py
@@ -98,3 +98,25 @@ def solution(numbers):
         if i not in numbers:
             answer.append(i)
     return sum(answer)
+
+# 신고 결과 받기
+from collections import defaultdict
+def solution(id_list, report, k):
+    answer=[]
+    report = list(set(report))
+    user = defaultdict(set)
+    cnt = defaultdict(int)
+    
+    for r in report:
+        a,b = r.split()
+        user[a].add(b)
+        cnt[b]+=1
+    
+    for i in id_list:
+        result = 0
+        for u in user[i]:
+            if cnt[u]>=k:
+                result += 1
+        answer.append(result)
+    return answer
+    
