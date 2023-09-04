8ef0f106e574a3cbb5b15f2c176df30027ebced2
Jung_Jihoon <coutinho10ace@gmail.com>
2023-09-04 15:24:05 +0900

    기사단원

--- Programmers/etc.py
+++ Programmers/etc.py
@@ -147,3 +147,41 @@ def solution(k, m, score):
             l.append(score.pop())
         answer += (min(l)*m)
     return answer
+# 기사단원의 무기(1) : 런타임 오류
+def solution(number, limit, power):
+    weak=[]
+    for i in range(1,number+1):
+        num=0
+        for j in range(1,i+1):
+            if i%j ==0:
+                num+=1
+        weak.append(num)
+    answer=[]
+    for k in weak:
+        if k > limit:
+            answer.append(power)
+        else:
+            answer.append(k)
+    real_answer=sum(answer)
+    return real_answer
+# 약수의 개수를 구할 때에 제곱근을 이용하여 2개씩 플러스
+def get_cds(n, limit , power):
+    cnt = 0
+    for i in range(1, int(n**(1/2))+1): # ★포인트1. 제곱근만큼만 반복
+        if n%i == 0:
+            if i == n//i: # 제곱근일 경우 -> 1개만 카운트하기
+                cnt += 1
+            else:
+                cnt += 2 # 제곱근이 아닐 경우, 2개 카운트 (i, n//i)
+        if cnt > limit:  # ★포인트2. 소수의 개수가 limit를 넘어가면
+            return power #            강제로 power만큼을 return 
+    return cnt
+
+    
+def solution(number, limit, power):
+    total = 1
+    for i in range(2, number+1):
+        len_cds = get_cds(i, limit, power)
+        total += len_cds
+
+    return total
