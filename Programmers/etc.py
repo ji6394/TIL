0076ac018e36eac8bb4412b3a15ec8ed7c3d9439
Jung_Jihoon <coutinho10ace@gmail.com>
2023-09-13 14:55:56 +0900

    개인정보수집유효기간

--- Programmers/etc.py
+++ Programmers/etc.py
@@ -237,3 +237,24 @@ def solution(t, p):
         if num <= int(p):
             answer+=1
     return answer
+# 개인정보 수집 유효기간
+def time_convert(t) :
+    year, month, day = map(int, t.split('.'))
+    return year * 12 * 28 + month * 28 + day
+
+def solution(today, terms, privacies):
+    term_dict = dict()
+    today = time_convert(today)
+    answer = []    
+    
+    for term in terms :
+        name, period = term.split()
+        term_dict[name] = int(period) * 28
+    
+    for idx, privacy in enumerate(privacies) :
+        start, name = privacy.split()
+        end = time_convert(start) + term_dict[name]
+        if end <= today :
+            answer.append(idx+1)    
+    
+    return answer
