3db63c927eb818aa5bcb0c67742fec1f12709b06
Jung_Jihoon <coutinho10ace@gmail.com>
2023-10-02 23:10:28 +0900

    덧칠하기

--- Programmers/etc.py
+++ Programmers/etc.py
@@ -278,4 +278,13 @@ def solution(cards1, cards2, goal):
             answer='No'
             break
     return answer
+# 덧칠하기
+def solution(n, m, section):
+    answer = 1 # 칠하는 횟수
+    paint = section[0] # 덧칠 시작점
+    for i in range(1, len(section)):
+        if section[i] - paint >= m:
+            answer += 1
+            paint = section[i]
             
+    return answer
